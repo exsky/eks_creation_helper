8ecdf15674bf01ce45fc90d1d3a7dea16f1a9d1c
Alex Yang <alex19911118@gmail.com>
2020-08-25 18:14:42 +0800

    fix: adjust the parameters format, set sg inboard rule

--- create_eks_cluster.sh
+++ create_eks_cluster.sh
@@ -30,9 +30,9 @@ SUBNET_ID_1=$(aws ec2 create-subnet --vpc-id $VPCID --cidr-block 192.168.2.0/24 
 SUBNET_ID_2=$(aws ec2 create-subnet --vpc-id $VPCID --cidr-block 192.168.1.0/24 --availability-zone-id apne1-az2 | grep "SubnetId" | awk -F "\"" '{ print $4}')
 SUBNET_ID_3=$(aws ec2 create-subnet --vpc-id $VPCID --cidr-block 192.168.0.0/24 --availability-zone-id apne1-az4 | grep "SubnetId" | awk -F "\"" '{ print $4}')
 
-echo "SUBNET_ID_1=${SUBNET_ID_1}" >> vpc_parameter.txt
-echo "SUBNET_ID_2=${SUBNET_ID_2}" >> vpc_parameter.txt
-echo "SUBNET_ID_3=${SUBNET_ID_3}" >> vpc_parameter.txt
+echo "SUBNET_ID_1=\"${SUBNET_ID_1}\"" >> vpc_parameter.txt
+echo "SUBNET_ID_2=\"${SUBNET_ID_2}\"" >> vpc_parameter.txt
+echo "SUBNET_ID_3=\"${SUBNET_ID_3}\"" >> vpc_parameter.txt
 
 # Enable auto-assign public IPv4 address
 aws ec2 modify-subnet-attribute --map-public-ip-on-launch --subnet-id $SUBNET_ID_1
@@ -40,7 +40,8 @@ aws ec2 modify-subnet-attribute --map-public-ip-on-launch --subnet-id $SUBNET_ID
 aws ec2 modify-subnet-attribute --map-public-ip-on-launch --subnet-id $SUBNET_ID_3
 
 SG_ID=$(aws ec2 create-security-group --group-name $SG_NAME --vpc-id $VPCID --description "Security Group for EKS" | grep "GroupId" | awk -F "\"" '{ print $4}')
-echo "SG_ID=${SG_ID}" >> vpc_parameter.txt
+aws ec2 authorize-security-group-ingress --group-id $SG_ID --protocol -1 --port -1 --cidr 0.0.0.0/0
+echo "SG_ID=\"${SG_ID}\"" >> vpc_parameter.txt
 
 CLUSTER_ROLE_ARN=$(aws iam get-role --role-name $CLUSTER_ROLE_NAME | grep "Arn"| awk -F "\"" '{ print $4}')
 
