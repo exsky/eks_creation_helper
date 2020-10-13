71db795d75d18b927ad15d67a30e0e881a12ce6b
Alex Yang <alex19911118@gmail.com>
2020-10-13 09:09:05 +0800

    :sparkles: feat: connect specific region by botocore.config

--- libs/connector.py
+++ libs/connector.py
@@ -1,10 +1,23 @@
 import boto3
+from botocore.config import Config
 
 
 class Connector:
+
+    # If you prefer using specific region rather than ~/.aws/credentials
+    config = Config(
+        region_name='ap-northeast-1',
+        signature_version='v4',
+        retries={
+            'max_appmpts': 10,
+            'mode': 'standard'
+        }
+    )
+
     def __init__(self, srv_name):
         self.srv_name = srv_name
         self.client = boto3.client(srv_name)
+        # self.client = boto3.client(srv_name, config=config)
 
     def connect_aws_service(self):
         return self.client
